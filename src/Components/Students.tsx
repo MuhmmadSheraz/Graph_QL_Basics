f7227e5ea599f36c82c29e7bb2c566ab10bfe436
MuhmmadSheraz <mshazy999@gmail.com>
2020-11-11 02:11:39 +0500

    mutation added

--- src/Components/Students.tsx
+++ src/Components/Students.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import { StudentType } from "./Types/Types";
 
-const alStudents = gql`
+const allStudents = gql`
   query {
     studentsData {
       name
@@ -12,26 +12,71 @@ const alStudents = gql`
     }
   }
 `;
+const ADD_STUDENT = gql`
+  mutation AddStudent($id: Int!, $email: String!, $age: Int!, $name: String!) {
+    addStudent(input: { id: $id, name: $name, email: $email, age: $age }) {
+      name
+      id
+      email
+      age
+    }
+  }
+`;
 const StudentsList = () => {
-  const { loading, error, data } = useQuery(alStudents);
+  const [name, setName] = useState<String>("");
+  const [age, setAge] = useState<number>(0);
+  const [email, setEmail] = useState<String>("");
+  const { loading, error, data } = useQuery(allStudents);
+  const [addStudentNow] = useMutation(ADD_STUDENT);
   if (loading) return <h1>Loading ....</h1>;
   if (error) return <h1>Error!!!</h1>;
   const { studentsData } = data;
   return (
     <div>
-      <h1 style={{textAlign:"center"}}>Students List ***</h1>
-      <hr/>
+      <h1 style={{ textAlign: "center" }}>Students List ***</h1>
+      <input
+        placeholder="Insert User Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Insert Age "
+        onChange={(e: React.FormEvent<HTMLInputElement>) =>
+          setAge(parseFloat(e.currentTarget.value))
+        }
+      />
+      <input
+        placeholder="Insert Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <hr />
       {studentsData &&
         studentsData.map((item: StudentType) => {
           return (
-            <div key={item.id} style={{marginLeft:"10px"}}>
+            <div key={item.id} style={{ marginLeft: "10px" }}>
               <h1>{item.name}</h1>
               <h1>{item.email}</h1>
               <h1>{item.age}</h1>
-              <hr/>
+              <hr />
             </div>
           );
         })}
+      <button
+        onClick={
+          () => 
+          addStudentNow({
+            variables: {
+              id: Math.floor(Math.random() * 1000),
+              email,
+              name,
+              age,
+            },
+            refetchQueries: [{ query: allStudents }]
+
+          })
+        }
+      >
+        Add More Student
+      </button>
     </div>
   );
 };
